ee95bcfe752dae3e524f143bf8727cc42949cff2
BelliaNino <bellianino1996@gmail.com>
2026-05-01 12:15:00 +0200

    add map

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -5,21 +5,21 @@ export default function AppCard({ languages }) {
 
     return (
         <>
-        <h1>Learn Web Development</h1>
+            <h1>Learn Web Development</h1>
 
-        <div className="container mt-4"> 
-            
-            <div className="card">
-                <div className="card-body">
-                    <button className='btn btn-primary'>HTML</button>
-                    <h2 className='card-title'>html</h2>
-                    <p className="card-text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolorum!
-                    </p>
-                </div>
+            <div className="container mt-4">
+                {languages.map(item => (
+                    <div className="card">
+                        <div className="card-body">
+                            <button className='btn btn-primary'>{item.title}</button>
+                            <p className="card-text">
+                                {item.description}
+                            </p>
+                        </div>
+                    </div>
+                ))
+                }
             </div>
-
-        </div>
         </>
     );
 }
