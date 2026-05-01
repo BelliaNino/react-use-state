57f985a0ebff951493dc7daddd37ceb9ff8a1ea6
BelliaNino <bellianino1996@gmail.com>
2026-05-01 12:01:38 +0200

    add layout

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,10 +1,25 @@
-export default function AppCard({languages}) {
+import { useState } from 'react';
 
+export default function AppCard({ languages }) {
+    const [openId, setOpenId] = useState(null);
 
     return (
-
         <>
-           <h1>use state</h1>
+        <h1>Learn Web Development</h1>
+
+        <div className="container mt-4"> 
+            
+            <div className="card">
+                <div className="card-body">
+                    <button className='btn btn-primary'>HTML</button>
+                    <h2 className='card-title'>html</h2>
+                    <p className="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolorum!
+                    </p>
+                </div>
+            </div>
+
+        </div>
         </>
-    )
+    );
 }
