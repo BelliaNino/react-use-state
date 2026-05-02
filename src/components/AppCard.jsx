96bd4ad871c8e13ffeb3b990b629a68a9fe9782f
BelliaNino <bellianino1996@gmail.com>
2026-05-02 11:47:51 +0200

    add logic

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,24 +1,54 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function AppCard({ languages }) {
-    const [openId, setOpenId] = useState(null);
+function Card({ title, description }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function clickHandler() {
+        setIsOpen(!isOpen);
+    }
 
     return (
+
+        <div className="card m-3">
+
+            <div className="card-body">
+
+                <button onClick={clickHandler} className={`btn ${isOpen ? "btn-warning" : "btn-primary"}`}>               
+                    {title}
+                </button>
+
+                {isOpen && (
+                    <p className="card-text mt-3">
+                        {description}
+                    </p>
+                )}
+
+            </div>
+
+        </div>
+    );
+}
+
+export default function AppCard({ languages }) {
+
+    return (
+
         <>
             <h1>Learn Web Development</h1>
 
             <div className="container mt-4">
-                {languages.map(item => (
-                    <div className="card">
-                        <div className="card-body">
-                            <button className='btn btn-primary'>{item.title}</button>
-                            <p className="card-text">
-                                {item.description}
-                            </p>
-                        </div>
-                    </div>
-                ))
-                }
+
+                {languages.map((item) => (
+
+                    <Card
+                        key={item.title}
+                        title={item.title}
+                        description={item.description}
+                    />
+
+                ))}
+
             </div>
         </>
     );
